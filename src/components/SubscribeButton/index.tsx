9ce75fb0715d92fb0462282a44218a4738333de8
Diogo Silva <diogosilvaoficial@MacBook-Pro-de-Diogo.local>
2021-12-24 03:48:12 -0300

    Fim Parte 1 Modulo Next.js

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./styles.module.scss";
+
+interface SubcribeButtonProps {
+  priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubcribeButtonProps) {
+  return (
+    <button type="button" className={styles.subscribeButton}>
+      Subscribe now
+    </button>
+  );
+}
